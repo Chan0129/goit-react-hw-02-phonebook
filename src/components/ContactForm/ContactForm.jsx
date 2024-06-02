fe62375dee4e5510472b65854fe59ca6728867f2
Chan0129 <142906874+Chan0129@users.noreply.github.com>
2024-06-02 21:58:31 +0800

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,30 +1,46 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
 
-export const ContactForm = ({ addContact, contacts }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
-  const handleNameChange = e => {
-    setName(e.target.value);
+export class ContactForm extends Component {
+  static propTypes = {
+    addContact: PropTypes.func.isRequired,
+    contacts: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        number: PropTypes.string.isRequired,
+      })
+    ),
   };
 
-  const handleNumberChange = e => {
-    setNumber(e.target.value);
+  state = {
+    name: '',
+    number: '',
   };
 
-  const handleSubmit = e => {
-    // prevent the form refreshing when submitting
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+
+  handleNumberChange = e => {
+    this.setState({
+      number: e.target.value,
+    });
+  };
+
+  handleSubmit = e => {
     e.preventDefault();
+    const { name, number } = this.state;
+    const { addContact, contacts } = this.props;
 
-    // if name and number is empty, it will not submit(return)
     if (name.trim() === '' || number.trim() === '') {
       return;
     }
 
-    // if existing contact set an alert, it will not submit(return)
     const existingContact = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
@@ -40,54 +56,46 @@ export const ContactForm = ({ addContact, contacts }) => {
       number: number.trim(),
     });
 
-    // Reset Form Fields upon submitting
-    setName('');
-    setNumber('');
+    this.setState({
+      name: '',
+      number: '',
+    });
   };
 
-  return (
-    <form className={css.form} onSubmit={handleSubmit}>
-      <label className={css.formField}>
-        <p className={css.formLabel}>Name</p>
-        <input
-          type="text"
-          name="name"
-          // add \ before - in [' \-] to make it work (LMS)
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
-          required
-          value={name}
-          onChange={handleNameChange}
-        />
-      </label>
+  render() {
+    const { name, number } = this.state;
 
-      <label className={css.formField}>
-        <p className={css.formLabel}>Number</p>
-        <input
-          type="tel"
-          name="number"
-          // add \ before - in [\-.\s] to make it work (LMS)
-          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={number}
-          onChange={handleNumberChange}
-        />
-      </label>
-      <button className={css.formButton} type="submit">
-        Add Contact
-      </button>
-    </form>
-  );
-};
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.formField}>
+          <p className={css.formLabel}>Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan."
+            required
+            value={name}
+            onChange={this.handleNameChange}
+          />
+        </label>
 
-ContactForm.propTypes = {
-  addContact: PropTypes.func.isRequired,
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-};
+        <label className={css.formField}>
+          <p className={css.formLabel}>Number</p>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={number}
+            onChange={this.handleNumberChange}
+          />
+        </label>
+        <button className={css.formButton} type="submit">
+          Add Contact
+        </button>
+      </form>
+    );
+  }
+}
